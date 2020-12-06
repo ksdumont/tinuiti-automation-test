ac947f7c2cbee6420d656e48f58951520078f3fd
Keith Dumont <keithdumont@Keiths-MBP.lan>
2020-12-06 07:53:13 -0800

    initial commit

---
+++ test.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+chrome_browser = webdriver.Chrome(ChromeDriverManager().install())
+
+chrome_browser.get(
+    'http://localhost:3000')
+
+
+query_search = chrome_browser.find_element_by_class_name('form-control')
+
+query_search.send_keys('emtrak')
+
+see_more_button = chrome_browser.find_element_by_class_name('card-button')
+
+see_more_button.click()
+
+go_back_button = chrome_browser.find_element_by_class_name(
+    'product-page-button')
+
+go_back_button.click()
+
+print('it seems to be working')
+
+chrome_browser.quit()
